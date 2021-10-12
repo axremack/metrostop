8da9edcacd542aac486bce024fab93b93d13563e
uke <ax.remack@gmail.com>
2021-10-12 09:42:54 +0200

    :wrench: Adding ressources and Junit library

---
+++ tests/ParserTest.java
@@ -0,0 +1,10 @@
+package parser;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+//@Test
+public class ParserTest extends TestCase {
+    public void testTest() {
+        assertTrue(true);
+    }
+}
