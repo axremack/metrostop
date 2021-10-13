78f0c7984437224af30743f5f4ae180db1defdeb
uke <ax.remack@gmail.com>
2021-10-13 11:05:28 +0200

    :sparkles: Adding functionnal Parser

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,20 @@
 package main;
 
+import parser.MetroStop;
+import parser.Parser;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        String filepath = "resources/ratp_arret.csv";
+        Parser p = new Parser(filepath);
+        List<MetroStop> list = new ArrayList<>();
+        p.parse(list);
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,8 +1,36 @@
 package parser;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
 
 public class Parser {
-    public void parse (){
+    private Reader reader;
+    private BufferedReader br;
 
+    // Constructeur
+    public Parser(String filepath){
+        try {
+            this.reader = new FileReader(filepath);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        br = new BufferedReader(this.reader);
     }
 
+    // Méthodes
+    public void parse(List<MetroStop> list_m) throws Exception {
+        String line = br.readLine();
+
+        while (line != null) {
+            String[] attrs = line.split("#");
+            if (attrs[5].equals("metro")) {
+                MetroStop m = new MetroStop(attrs);
+                list_m.add(m);
+            }
+            line = br.readLine();
+        }
+        br.close();
+    }
 }
