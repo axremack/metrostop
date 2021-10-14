9aade122d203b5f549c7917386baf74b93124832
uke <ax.remack@gmail.com>
2021-10-14 19:39:36 +0200

    :white_check_mark: Testing MetroStop sorting by name and by district

--- tests/MetroStopManagerTest.java
+++ tests/MetroStopManagerTest.java
@@ -9,15 +9,16 @@ import java.util.List;
 public class MetroStopManagerTest extends TestCase {
     private List<MetroStop> listMetro = new ArrayList<>();
 
+    private MetroStop m1 = new MetroStop(1975,2.33871281165883,48.8844176451841,"Abbesses","PARIS-18EME","metro");
+    private MetroStop m2 = new MetroStop(1981,2.32674567371924,48.828398514348,"Aalésia","PARIS-14EME","metro");
+    private MetroStop m3 = new MetroStop(1978,2.3949898158233,48.8561744489676,"Alexandre-Dumas","PARIS-11EME","metro");
+
+
     // Les deux méthodes suivantes servent à empêcher les effets de bord
     @Override
     protected void setUp () throws Exception {
         super.setUp ();
         listMetro = new ArrayList<>();
-
-        MetroStop m1 = new MetroStop(1975,2.33871281165883,48.8844176451841,"Abbesses","PARIS-18EME","metro");
-        MetroStop m2 = new MetroStop(1981,2.32674567371924,48.828398514348,"Alésia","PARIS-14EME","metro");
-        MetroStop m3 = new MetroStop(1978,2.3949898158233,48.8561744489676,"Alexandre-Dumas","PARIS-11EME","metro");
 
         listMetro.add(m1);
         listMetro.add(m2);
@@ -30,14 +31,13 @@ public class MetroStopManagerTest extends TestCase {
         listMetro = null ;
     }
 
+    // Tests de l'affichage d'une liste de MetroStop
+
+
     // Tests des méthodes de tri
     @Test
-    public void testTrierParID() {
+    public void testSortById() {
         List<MetroStop> listTest = new ArrayList<>();
-
-        MetroStop m1 = new MetroStop(1975,2.33871281165883,48.8844176451841,"Abbesses","PARIS-18EME","metro");
-        MetroStop m2 = new MetroStop(1981,2.32674567371924,48.828398514348,"Alésia","PARIS-14EME","metro");
-        MetroStop m3 = new MetroStop(1978,2.3949898158233,48.8561744489676,"Alexandre-Dumas","PARIS-11EME","metro");
 
         // Ajout des MetroStop dans l'ordre croissant des identifiants
         listTest.add(m1);
@@ -45,6 +45,32 @@ public class MetroStopManagerTest extends TestCase {
         listTest.add(m2);
 
         MetroStopManager.sortById(listMetro);
+        assertEquals(listTest, listMetro);
+    }
+
+    @Test
+    public void testSortByDistrict() {
+        List<MetroStop> listTest = new ArrayList<>();
+
+        // Ajout des MetroStop dans l'ordre alphabétique des arrondissements
+        listTest.add(m3);
+        listTest.add(m2);
+        listTest.add(m1);
+
+        MetroStopManager.sortByDistrict(listMetro);
+        assertEquals(listTest, listMetro);
+    }
+
+    @Test
+    public void testSortByName() {
+        List<MetroStop> listTest = new ArrayList<>();
+
+        // Ajout des MetroStop dans l'ordre alphabétique des noms
+        listTest.add(m2);
+        listTest.add(m1);
+        listTest.add(m3);
+
+        MetroStopManager.sortByName(listMetro);
         assertEquals(listTest, listMetro);
     }
 
