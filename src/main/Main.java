6e8945f2f41606fec7e7d3c33bca368e99239a26
uke <ax.remack@gmail.com>
2021-10-17 19:13:04 +0200

    :sparkles: Adding the menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,8 +6,17 @@ import parser.Parser;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    private static void printMenu() {
+        System.out.println("Quelle fonction éxecuter ?");
+        System.out.println("\t1 - Afficher la liste des arrêts de métro.");
+        System.out.println("\t2 - Trier la liste des arrêts de métro par identifiant.");
+        System.out.println("\t3 - Trier la liste des arrêts de métro par arrondissement (puis nom).");
+        System.out.println("\t4 - Arrêter le programme.");
+    }
+
     public static void main(String[] args) throws Exception {
         String filepath = "resources/ratp_arret.csv";
         Parser p = new Parser(filepath);
@@ -15,15 +24,38 @@ public class Main {
         p.parse(list);
 
         MetroStopManager manager = new MetroStopManager();
-        manager.printMetroStopList(list);
-        System.out.println();
-        System.out.println();
-        manager.sortByName(list);
-        manager.printMetroStopList(list);
+        Scanner sc = new Scanner(System.in);
 
+        boolean fin = false;
 
+        while (!fin) {
+            printMenu();
+            String rep = sc.nextLine();
+            System.out.println();
 
-
-
+            switch (rep) {
+                // Affichage de la liste des arrêts de métro
+                case "1":
+                    manager.printMetroStopList(list);
+                    System.out.println();
+                    break;
+                // Tri par identifiant puis affichage de la liste
+                case "2":
+                    manager.sortById(list);
+                    manager.printMetroStopList(list);
+                    break;
+                // Tri par arrondissement (puis nom) puis affichage de la liste
+                case "3":
+                    manager.sortByDistrict(list);
+                    manager.printMetroStopList(list);
+                    break;
+                // Terminer le programme
+                default:
+                    System.out.println("Adios");
+                    fin = true;
+                    break;
+            }
+        }
+        sc.close();
     }
 }
